6d2aedd272a7d95229797a1675e247fd6b038203
Markco <hk106b@gmail.com>
2021-09-08 18:40:27 +0800

    basic version

---
+++ guess.py
@@ -0,0 +1,13 @@
+import random 
+
+r = random.randint(1, 100)
+while True:
+	num = input('請猜數字： ') # input 都會把所有野存成字串
+	num = int(num) # casting
+	if num == r:
+		print('終於猜對了！')
+		break
+	elif num > r:
+		print('比答案大')
+	elif num < r:
+		print('比答案小')
