48862e8f5b39299cf6f6f77e5e4fcc7770de970f
Markco <hk106b@gmail.com>
2021-09-08 22:12:54 +0800

    add count

--- guess.py
+++ guess.py
@@ -1,13 +1,17 @@
 import random 
 
 r = random.randint(1, 100)
+count = 0 
 while True:
+	count += 1 # count = count + 1 
 	num = input('請猜數字： ') # input 都會把所有野存成字串
 	num = int(num) # casting
 	if num == r:
 		print('終於猜對了！')
+		print('這是你猜既第', count, '次')
 		break
 	elif num > r:
 		print('太大拉')
 	elif num < r:
 		print('太細拉')
+	print('這是你猜既第', count, '次')
